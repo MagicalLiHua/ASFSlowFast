d011b6b556ee8a16ae0397ee26d39c529a64e338
L1Hua <117365538+MagicalLiHua@users.noreply.github.com>
2024-11-22 18:49:54 +0800

    Add files via upload

--- models/base_modules.py
+++ models/base_modules.py
@@ -1,104 +1,228 @@
 import torch
 import torch.nn as nn
-from typing import Tuple
+from typing import Optional, List, Tuple
 
 
-class BasicConv3D(nn.Module):
-    """Basic 3D convolution block"""
+def channel_shuffle(x, groups):
+    '''Channel shuffle operation
+    Channel shuffle: [N,C,H,W] -> [N,g,C/g,H,W] -> [N,C/g,g,H,w] -> [N,C,H,W]'''
+    batchsize, num_channels, depth, height, width = x.data.size()
+    channels_per_group = num_channels // groups
+    x = x.view(batchsize, groups,
+               channels_per_group, depth, height, width)
+    x = x.permute(0, 2, 1, 3, 4, 5).contiguous()
+    x = x.view(batchsize, num_channels, depth, height, width)
+    return x
 
+
+class MobileOneBlock(nn.Module):
     def __init__(self,
                  in_channels: int,
                  out_channels: int,
-                 kernel_size: Tuple[int, int, int],
-                 stride: Tuple[int, int, int] = (1, 1, 1),
-                 padding: Tuple[int, int, int] = (0, 0, 0),
+                 kernel_size: int,
+                 stride: Tuple = (1, 1, 1),
+                 padding: int = 0,
+                 dilation: int = 1,
                  groups: int = 1,
-                 bias: bool = False):
-        super(BasicConv3D, self).__init__()
-        self.conv = nn.Conv3d(
-            in_channels,
-            out_channels,
-            kernel_size=kernel_size,
-            stride=stride,
-            padding=padding,
-            groups=groups,
-            bias=bias
-        )
-        self.bn = nn.BatchNorm3d(out_channels)
-        self.relu = nn.ReLU(inplace=True)
-
-    def forward(self, x):
-        x = self.conv(x)
-        x = self.bn(x)
-        x = self.relu(x)
-        return x
-
-
-class ChannelShuffle(nn.Module):
-    """Channel shuffle operation"""
-
-    def __init__(self, groups):
-        super(ChannelShuffle, self).__init__()
+                 inference_mode: bool = False,
+                 use_se: bool = False,
+                 num_conv_branches: int = 1) -> None:
+        super(MobileOneBlock, self).__init__()
+        self.inference_mode = inference_mode
         self.groups = groups
+        self.stride = stride
+        self.kernel_size = kernel_size
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.num_conv_branches = num_conv_branches
 
-    def forward(self, x):
-        batchsize, num_channels, depth, height, width = x.size()
-        channels_per_group = num_channels // self.groups
+        self.se = nn.Identity()
+        self.activation = nn.ReLU()
 
-        # Reshape
-        x = x.view(batchsize, self.groups, channels_per_group, depth, height, width)
+        if inference_mode:
+            self.reparam_conv = nn.Conv3d(in_channels=in_channels,
+                                          out_channels=out_channels,
+                                          kernel_size=kernel_size,
+                                          stride=stride,
+                                          padding=padding,
+                                          dilation=dilation,
+                                          groups=groups,
+                                          bias=True)
+        else:
+            self.rbr_skip = nn.BatchNorm3d(num_features=in_channels) \
+                if out_channels == in_channels and stride == (1, 1, 1) else None
 
-        # Permute
-        x = x.permute(0, 2, 1, 3, 4, 5).contiguous()
+            rbr_conv = list()
+            for _ in range(self.num_conv_branches):
+                rbr_conv.append(self._conv_bn(kernel_size=kernel_size,
+                                              padding=padding))
+            self.rbr_conv = nn.ModuleList(rbr_conv)
 
-        # Flatten
-        x = x.view(batchsize, num_channels, depth, height, width)
+            self.rbr_scale = None
+            if kernel_size > 1:
+                self.rbr_scale = self._conv_bn(kernel_size=1,
+                                               padding=0)
 
-        return x
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        if self.inference_mode:
+            return self.activation(self.se(self.reparam_conv(x)))
+
+        identity_out = 0
+        if self.rbr_skip is not None:
+            identity_out = self.rbr_skip(x)
+
+        scale_out = 0
+        if self.rbr_scale is not None:
+            scale_out = self.rbr_scale(x)
+
+        out = scale_out + identity_out
+        for ix in range(self.num_conv_branches):
+            out += self.rbr_conv[ix](x)
+
+        return self.activation(self.se(out))
+
+    def reparameterize(self):
+        if self.inference_mode:
+            return
+        kernel, bias = self._get_kernel_bias()
+        self.reparam_conv = nn.Conv3d(in_channels=self.rbr_conv[0].conv.in_channels,
+                                      out_channels=self.rbr_conv[0].conv.out_channels,
+                                      kernel_size=self.rbr_conv[0].conv.kernel_size,
+                                      stride=self.rbr_conv[0].conv.stride,
+                                      padding=self.rbr_conv[0].conv.padding,
+                                      dilation=self.rbr_conv[0].conv.dilation,
+                                      groups=self.rbr_conv[0].conv.groups,
+                                      bias=True)
+        self.reparam_conv.weight.data = kernel
+        self.reparam_conv.bias.data = bias
+
+        for para in self.parameters():
+            para.detach_()
+        self.__delattr__('rbr_conv')
+        self.__delattr__('rbr_scale')
+        if hasattr(self, 'rbr_skip'):
+            self.__delattr__('rbr_skip')
+
+        self.inference_mode = True
+
+    def _get_kernel_bias(self) -> Tuple[torch.Tensor, torch.Tensor]:
+        kernel_scale = 0
+        bias_scale = 0
+        if self.rbr_scale is not None:
+            kernel_scale, bias_scale = self._fuse_bn_tensor(self.rbr_scale)
+            pad = self.kernel_size // 2
+            kernel_scale = torch.nn.functional.pad(kernel_scale,
+                                                   [pad, pad, pad, pad, pad, pad])
+
+        kernel_identity = 0
+        bias_identity = 0
+        if self.rbr_skip is not None:
+            kernel_identity, bias_identity = self._fuse_bn_tensor(self.rbr_skip)
+
+        kernel_conv = 0
+        bias_conv = 0
+        for ix in range(self.num_conv_branches):
+            _kernel, _bias = self._fuse_bn_tensor(self.rbr_conv[ix])
+            kernel_conv += _kernel
+            bias_conv += _bias
+
+        kernel_final = kernel_conv + kernel_scale + kernel_identity
+        bias_final = bias_conv + bias_scale + bias_identity
+        return kernel_final, bias_final
+
+    def _fuse_bn_tensor(self, branch) -> Tuple[torch.Tensor, torch.Tensor]:
+        if isinstance(branch, nn.Sequential):
+            kernel = branch.conv.weight
+            running_mean = branch.bn.running_mean
+            running_var = branch.bn.running_var
+            gamma = branch.bn.weight
+            beta = branch.bn.bias
+            eps = branch.bn.eps
+        else:
+            assert isinstance(branch, nn.BatchNorm3d)
+            if not hasattr(self, 'id_tensor'):
+                input_dim = self.in_channels // self.groups
+                kernel_value = torch.zeros((self.in_channels,
+                                            input_dim,
+                                            self.kernel_size,
+                                            self.kernel_size,
+                                            self.kernel_size),
+                                           dtype=branch.weight.dtype,
+                                           device=branch.weight.device)
+                for i in range(self.in_channels):
+                    kernel_value[i, i % input_dim,
+                                    self.kernel_size // 2,
+                                    self.kernel_size // 2,
+                                    self.kernel_size // 2] = 1
+                self.id_tensor = kernel_value
+            kernel = self.id_tensor
+            running_mean = branch.running_mean
+            running_var = branch.running_var
+            gamma = branch.weight
+            beta = branch.bias
+            eps = branch.eps
+        std = (running_var + eps).sqrt()
+        t = (gamma / std).reshape(-1, 1, 1, 1, 1)
+        return kernel * t, beta - running_mean * gamma / std
+
+    def _conv_bn(self,
+                 kernel_size: int,
+                 padding: int) -> nn.Sequential:
+        mod_list = nn.Sequential()
+        mod_list.add_module('conv', nn.Conv3d(in_channels=self.in_channels,
+                                              out_channels=self.out_channels,
+                                              kernel_size=kernel_size,
+                                              stride=self.stride,
+                                              padding=padding,
+                                              groups=self.groups,
+                                              bias=False))
+        mod_list.add_module('bn', nn.BatchNorm3d(num_features=self.out_channels))
+        return mod_list
 
 
-class FastToSlowFusion(nn.Module):
-    """Basic fusion module from Fast to Slow pathway"""
-
+class MobileOneBlocklist(nn.Module):
     def __init__(self,
-                 in_channels: int,
-                 fusion_kernel: int = 5,
-                 alpha: int = 8):
-        super(FastToSlowFusion, self).__init__()
-        self.conv = nn.Conv3d(
-            in_channels,
-            in_channels * 2,
-            kernel_size=(fusion_kernel, 1, 1),
-            stride=(alpha, 1, 1),
-            padding=(fusion_kernel // 2, 0, 0),
-            bias=False
-        )
-        self.bn = nn.BatchNorm3d(in_channels * 2)
-        self.relu = nn.ReLU(inplace=True)
+                 in_planes: int,
+                 out_planes: int,
+                 num_blocks: int = 1,
+                 stride: Tuple = (1, 1, 1),
+                 use_se: bool = False,
+                 inference_mode: bool = False,
+                 num_conv_branches: int = 1,
+                 num_se_blocks: int = 0):
+        super(MobileOneBlocklist, self).__init__()
 
-    def forward(self, x):
-        x = self.conv(x)
-        x = self.bn(x)
-        x = self.relu(x)
-        return x
+        self.GConv1 = MobileOneBlock(in_channels=in_planes,
+                                     out_channels=in_planes,
+                                     kernel_size=1,
+                                     stride=(1, 1, 1),
+                                     padding=0,
+                                     groups=1,
+                                     inference_mode=inference_mode,
+                                     use_se=use_se,
+                                     num_conv_branches=num_conv_branches)
+        self.DWConv = MobileOneBlock(in_channels=in_planes,
+                                     out_channels=in_planes,
+                                     kernel_size=3,
+                                     stride=stride,
+                                     padding=1,
+                                     groups=in_planes,
+                                     inference_mode=inference_mode,
+                                     use_se=use_se,
+                                     num_conv_branches=num_conv_branches)
+        self.GConv2 = MobileOneBlock(in_channels=in_planes,
+                                     out_channels=out_planes,
+                                     kernel_size=1,
+                                     stride=(1, 1, 1),
+                                     padding=0,
+                                     groups=1,
+                                     inference_mode=inference_mode,
+                                     use_se=use_se,
+                                     num_conv_branches=num_conv_branches)
 
-
-# Basic building block for pathway
-class PathwayBlock(nn.Module):
-    def __init__(self,
-                 in_channels: int,
-                 out_channels: int,
-                 stride: Tuple[int, int, int] = (1, 1, 1)):
-        super(PathwayBlock, self).__init__()
-        self.conv1 = BasicConv3D(in_channels, out_channels,
-                                 kernel_size=(1, 1, 1),
-                                 stride=(1, 1, 1))
-        self.conv2 = BasicConv3D(out_channels, out_channels,
-                                 kernel_size=(3, 3, 3),
-                                 stride=stride,
-                                 padding=(1, 1, 1))
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.conv2(x)
-        return x
+    def forward(self, input):
+        out = self.GConv1(input)
+        out = channel_shuffle(out, 2)
+        out = self.DWConv(out)
+        out = self.GConv2(out)
+        return out
--- models/slowfast_base.py
+++ models/slowfast_base.py
@@ -1,179 +1,179 @@
 import torch
 import torch.nn as nn
-from typing import Tuple, List
-from base_modules import BasicConv3D, ChannelShuffle, FastToSlowFusion, PathwayBlock
+from models.base_modules import MobileOneBlocklist
 
 
-class SlowFastBase(nn.Module):
-    """
-    Basic implementation of SlowFast networks.
+class BasicConv3D(nn.Module):
+    """Basic 3D convolution block for replacing Depthwise_Separable"""
 
-    Args:
-        num_classes (int): Number of classes for classification
-        dropout (float): Dropout rate
-        width_mult (float): Width multiplier for network channels
-    """
-
-    def __init__(self,
-                 num_classes: int = 400,
-                 dropout: float = 0.5,
-                 width_mult: float = 1.0):
-        super(SlowFastBase, self).__init__()
-
-        # Fast pathway
-        self.fast_conv1 = BasicConv3D(3, int(8 * width_mult),
-                                      kernel_size=(5, 7, 7),
-                                      stride=(1, 2, 2),
-                                      padding=(2, 3, 3))
-        self.fast_maxpool = nn.MaxPool3d(kernel_size=(1, 3, 3),
-                                         stride=(1, 2, 2),
-                                         padding=(0, 1, 1))
-
-        # Fast pathway stages
-        self.fast_res2 = self._make_stage_fast(int(8 * width_mult),
-                                               int(32 * width_mult),
-                                               stride=(1, 2, 2))
-        self.fast_res3 = self._make_stage_fast(int(32 * width_mult),
-                                               int(64 * width_mult),
-                                               stride=(1, 2, 2))
-        self.fast_res4 = self._make_stage_fast(int(64 * width_mult),
-                                               int(128 * width_mult),
-                                               stride=(1, 2, 2))
-        self.fast_res5 = self._make_stage_fast(int(128 * width_mult),
-                                               int(256 * width_mult),
-                                               stride=(1, 1, 1))
-
-        # Lateral connections
-        self.lateral1 = FastToSlowFusion(int(8 * width_mult))
-        self.lateral2 = FastToSlowFusion(int(32 * width_mult))
-        self.lateral3 = FastToSlowFusion(int(64 * width_mult))
-        self.lateral4 = FastToSlowFusion(int(128 * width_mult))
-
-        # Slow pathway
-        self.slow_conv1 = BasicConv3D(3, int(64 * width_mult),
-                                      kernel_size=(1, 7, 7),
-                                      stride=(1, 2, 2),
-                                      padding=(0, 3, 3))
-        self.slow_maxpool = nn.MaxPool3d(kernel_size=(1, 3, 3),
-                                         stride=(1, 2, 2),
-                                         padding=(0, 1, 1))
-
-        # Slow pathway stages
-        self.slow_res2 = self._make_stage_slow(int(64 * width_mult),
-                                               int(256 * width_mult),
-                                               stride=(1, 2, 2))
-        self.slow_res3 = self._make_stage_slow(int(256 * width_mult),
-                                               int(512 * width_mult),
-                                               stride=(1, 2, 2))
-        self.slow_res4 = self._make_stage_slow(int(512 * width_mult),
-                                               int(1024 * width_mult),
-                                               stride=(1, 2, 2))
-        self.slow_res5 = self._make_stage_slow(int(1024 * width_mult),
-                                               int(2048 * width_mult),
-                                               stride=(1, 1, 1))
-
-        # Final layers
-        self.dropout = nn.Dropout(dropout)
-        self.avgpool = nn.AdaptiveAvgPool3d(1)
-        self.fc = nn.Linear(int(256 * width_mult + 2048 * width_mult),
-                            num_classes)
-
-    def _make_stage_fast(self,
-                         in_channels: int,
-                         out_channels: int,
-                         stride: Tuple[int, int, int]) -> nn.Sequential:
-        """Create a stage of fast pathway"""
-        return nn.Sequential(
-            PathwayBlock(in_channels, out_channels, stride)
+    def __init__(self, in_ch, out_ch, kernel_size=1, stride=1, padding=0, bias=False):
+        super(BasicConv3D, self).__init__()
+        self.conv = nn.Conv3d(
+            in_channels=in_ch,
+            out_channels=out_ch,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            bias=bias
         )
+        self.bn = nn.BatchNorm3d(out_ch)
+        self.relu = nn.ReLU(inplace=True)
 
-    def _make_stage_slow(self,
-                         in_channels: int,
-                         out_channels: int,
-                         stride: Tuple[int, int, int]) -> nn.Sequential:
-        """Create a stage of slow pathway"""
-        return nn.Sequential(
-            PathwayBlock(in_channels, out_channels, stride)
+    def forward(self, input):
+        out = self.conv(input)
+        out = self.bn(out)
+        out = self.relu(out)
+        return out
+
+
+class BasicLateral(nn.Module):
+    """Basic lateral connection block"""
+
+    def __init__(self, channels):
+        super(BasicLateral, self).__init__()
+        self.conv = nn.Conv3d(
+            in_channels=channels,
+            out_channels=channels * 2,
+            kernel_size=(5, 1, 1),
+            stride=(4, 1, 1),
+            padding=(2, 0, 0),
+            bias=False
         )
+        self.bn = nn.BatchNorm3d(channels * 2)
+        self.relu = nn.ReLU(inplace=True)
 
-    def forward_fast(self, x) -> Tuple[torch.Tensor, List[torch.Tensor]]:
-        """Forward pass for fast pathway"""
-        lateral = []
+    def forward(self, x):
+        out = self.conv(x)
+        out = self.bn(out)
+        out = self.relu(out)
+        return out
 
-        x = self.fast_conv1(x)
-        x = self.fast_maxpool(x)
-        lateral.append(self.lateral1(x))
 
-        x = self.fast_res2(x)
-        lateral.append(self.lateral2(x))
+# Default parameters
+default_params = {
+    'width_multipliers': (0.5, 0.5, 0.5, 0.5),
+    'num_feature': 6,
+    'lateral': False,
+    'num_classes': 12,
+    'actions_classes': 6,
+    'users_classes': 15,
+    'epoch_num': 150,
+    'batch_size': 16,
+    'learning_rate': 0.01,
+}
 
-        x = self.fast_res3(x)
-        lateral.append(self.lateral3(x))
 
-        x = self.fast_res4(x)
-        lateral.append(self.lateral4(x))
+class SlowFast(nn.Module):
+    def __init__(self, actclass_num=default_params['actions_classes'],
+                 userclass_num=default_params['users_classes'],
+                 dropout=0.5,
+                 width_multipliers=default_params['width_multipliers'],
+                 C_multipliers=(0.5,)):
+        super(SlowFast, self).__init__()
 
-        x = self.fast_res5(x)
-        x = self.avgpool(x)
-        x = x.view(-1, x.size(1))
+        # Fast path definition
+        self.fast_conv1 = BasicConv3D(3, 8, kernel_size=(5, 7, 7), stride=(1, 2, 2), padding=(2, 3, 3), bias=False)
+        self.fast_maxpool = nn.MaxPool3d(kernel_size=(1, 3, 3), stride=(1, 2, 2), padding=(0, 1, 1))
 
-        return x, lateral
+        # Fast path main blocks
+        self.fast_shu2_1 = MobileOneBlocklist(in_planes=8, out_planes=int(32 * width_multipliers[0]), stride=(1, 2, 2))
+        self.fast_shu3_1 = MobileOneBlocklist(int(32 * width_multipliers[0]), out_planes=int(64 * width_multipliers[1]),
+                                              stride=(1, 2, 2))
+        self.fast_shu4_1 = MobileOneBlocklist(in_planes=int(64 * width_multipliers[1]),
+                                              out_planes=int(128 * width_multipliers[2]), stride=(1, 2, 2))
+        self.fast_shu5 = MobileOneBlocklist(in_planes=int(128 * width_multipliers[2]),
+                                            out_planes=int(256 * width_multipliers[3]))
 
-    def forward_slow(self, x, lateral) -> torch.Tensor:
-        """Forward pass for slow pathway"""
-        x = self.slow_conv1(x)
+        # Lateral connections with basic implementation
+        self.lateral_p1 = BasicLateral(8)
+        self.lateral_res2 = BasicLateral(int(32 * width_multipliers[0]))
+        self.lateral_res3 = BasicLateral(int(64 * width_multipliers[1]))
+        self.lateral_res4 = BasicLateral(int(128 * width_multipliers[2]))
+
+        # Slow path definition
+        self.slow_conv1 = BasicConv3D(3, int(64 * C_multipliers[0]), kernel_size=(1, 7, 7), stride=(1, 2, 2),
+                                      padding=(0, 3, 3), bias=False)
+        self.slow_maxpool = nn.MaxPool3d(kernel_size=(1, 3, 3), stride=(1, 2, 2), padding=(0, 1, 1))
+
+        # Slow path main blocks
+        self.slow_shu2_1 = MobileOneBlocklist(in_planes=int(16 + (64 * C_multipliers[0])),
+                                              out_planes=int(256 * width_multipliers[0] * C_multipliers[0]),
+                                              stride=(1, 2, 2))
+        self.slow_shu3_1 = MobileOneBlocklist(
+            in_planes=int((64 * width_multipliers[0]) + 256 * width_multipliers[0] * C_multipliers[0]),
+            out_planes=int(512 * width_multipliers[1] * C_multipliers[0]),
+            stride=(1, 2, 2))
+        self.slow_shu4_1 = MobileOneBlocklist(
+            in_planes=int((128 * width_multipliers[1]) + 512 * width_multipliers[0] * C_multipliers[0]),
+            out_planes=int(1024 * width_multipliers[2] * C_multipliers[0]),
+            stride=(1, 2, 2))
+        self.slow_shu5 = MobileOneBlocklist(
+            in_planes=int((256 * width_multipliers[2]) + 1024 * width_multipliers[2] * C_multipliers[0]),
+            out_planes=int(2048 * width_multipliers[3] * C_multipliers[0]))
+
+        # Output layers
+        self.dp = nn.Dropout(dropout)
+        self.fcx = nn.Linear(int(256 * width_multipliers[3] + (2048 * width_multipliers[3] * C_multipliers[0])),
+                             actclass_num, bias=True)
+        self.fcu = nn.Linear(int(256 * width_multipliers[3] + (2048 * width_multipliers[3] * C_multipliers[0])),
+                             userclass_num, bias=True)
+
+    def forward(self, input):
+        # Process fast pathway
+        fast, lateral = self.FastPath(input[:, :, ::4, :, :])  # temporal stride 4
+        # Process slow pathway
+        slow = self.SlowPath(input[:, :, ::16, :, :], lateral)  # temporal stride 16
+        # Combine pathways
+        x = torch.cat([slow, fast], dim=1)
+
+        # Output heads
+        a = self.dp(x)
+        a = self.fcx(a)
+
+        return a
+
+    def SlowPath(self, input, lateral):
+        x = self.slow_conv1(input)
         x = self.slow_maxpool(x)
         x = torch.cat([x, lateral[0]], dim=1)
 
-        x = self.slow_res2(x)
+        x = self.slow_shu2_1(x)
         x = torch.cat([x, lateral[1]], dim=1)
 
-        x = self.slow_res3(x)
+        x = self.slow_shu3_1(x)
         x = torch.cat([x, lateral[2]], dim=1)
 
-        x = self.slow_res4(x)
+        x = self.slow_shu4_1(x)
         x = torch.cat([x, lateral[3]], dim=1)
 
-        x = self.slow_res5(x)
-        x = self.avgpool(x)
+        x = self.slow_shu5(x)
+        x = nn.AdaptiveAvgPool3d(1)(x)
+        x = x.view(-1, x.size(1))
+        return x
+
+    def FastPath(self, input):
+        lateral = []
+
+        x = self.fast_conv1(input)
+        pool1 = self.fast_maxpool(x)
+
+        lateral_p = self.lateral_p1(pool1)
+        lateral.append(lateral_p)  # First fusion
+
+        shuf2_1 = self.fast_shu2_1(pool1)
+        lateral_res2 = self.lateral_res2(shuf2_1)
+        lateral.append(lateral_res2)  # Second fusion
+
+        shuf3_1 = self.fast_shu3_1(shuf2_1)
+        lateral_res3 = self.lateral_res3(shuf3_1)
+        lateral.append(lateral_res3)  # Third fusion
+
+        shuf4_1 = self.fast_shu4_1(shuf3_1)
+        lateral_res4 = self.lateral_res4(shuf4_1)
+        lateral.append(lateral_res4)  # Fourth fusion
+
+        shuf5 = self.fast_shu5(shuf4_1)
+        x = nn.AdaptiveAvgPool3d(1)(shuf5)
         x = x.view(-1, x.size(1))
 
-        return x
-
-    def forward(self, x):
-        # Fast pathway - temporal stride 4
-        fast_input = x[:, :, ::4, :, :]
-        fast_output, lateral = self.forward_fast(fast_input)
-
-        # Slow pathway - temporal stride 16
-        slow_input = x[:, :, ::16, :, :]
-        slow_output = self.forward_slow(slow_input, lateral)
-
-        # Combine pathways
-        x = torch.cat([slow_output, fast_output], dim=1)
-
-        # Final layers
-        x = self.dropout(x)
-        x = self.fc(x)
-
-        return x
-
-
-def create_slowfast_base(num_classes=400, dropout=0.5, width_mult=1.0):
-    """
-    Create a basic SlowFast network.
-
-    Args:
-        num_classes (int): Number of classes for classification
-        dropout (float): Dropout rate
-        width_mult (float): Width multiplier for network channels
-
-    Returns:
-        SlowFastBase: A basic SlowFast network
-    """
-    model = SlowFastBase(
-        num_classes=num_classes,
-        dropout=dropout,
-        width_mult=width_mult
-    )
-    return model
+        return x, lateral
